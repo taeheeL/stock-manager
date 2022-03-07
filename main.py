7a6550c779533b74d0eaedc08bc55dd4f2946c14
TaeHee Lee <t9h9@naver.com>
2022-03-07 16:01:38 +0900

    env commit

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ stock_params = {
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
 data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
-yesterday_data = data_list[1]
+yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 
-day_before_yesterday_data = data_list[2]
+day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 
 difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
